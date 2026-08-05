ca2adad97d767bdfa29bb4597ae735de4ddbd5de
Qian Yun <oldk1331@gmail.com>
2026-08-05 21:43:11 +0800

    use weak pointer

--- src/lisp/primitives.lisp
+++ src/lisp/primitives.lisp
@@ -928,21 +928,3 @@
     (sb-ext:weak-pointer-value weak-pointer)
     #-(or :clisp :cmu :ecl :sbcl)
     weak-pointer)
-
-(defun COMPACT_WEAK_ARRAY (array size default_element)
-    ;; return the size of array after compact
-    #+(or :clisp :cmu :ecl :sbcl)
-    (progn
-        (dotimes (i size)
-            (setf (aref array i) (MAKE_WEAK_POINTER (aref array i))))
-        (fullgc)
-        (do ((i 0 (1+ i)) (j 0))
-            ((= i size) j)
-            (multiple-value-bind (value valid)
-                (WEAK_POINTER_VALUE (aref array i))
-              (setf (aref array i) default_element)
-              (if valid (progn (setf (aref array j) value)
-                               (setq j (1+ j)))))))
-
-    #-(or :clisp :cmu :ecl :sbcl)
-    size)
